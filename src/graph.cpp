4e49a7c25e610e7bf00cd8eb1721332c1cbe36dd
trevorbelmont <belmont.programmer@gmail.com>
2023-11-04 10:24:39 -0300

    Corrige função checkGreedy \n (passa em 85% dos testes do VPL de avaliação)

--- src/graph.cpp
+++ src/graph.cpp
@@ -87,35 +87,30 @@ bool Grafo::CheckGreedy(int ord[][2]) {
   int i = 0;
   while (i < QuantidadeVertices()) {         // checa se todos os vértices ordenados satisfazem condição
     int corMax = ord[i][1];                  // a cor máxima (a cor do vértice i)
-    bool neighborColours[corMax];            // array de booleanas para check-list de cada cor inferior
     List* vizin = vertices->get(ord[i][0]);  // ponteiro para a lista de adjacência do i-ésimo vértice no array ordenado
-    // checa se todas as cores  inferiores a cor do vértice i, estão presentes nos vizinhos de i
-    // Se qualquer uma das cores inferiores não estiver presente, retorna falso imediatamente e cessa a checagem
+                                             // checa se todas as cores  inferiores a cor do vértice i, estão presentes nos vizinhos de i
+                                             // Se qualquer uma das cores inferiores não estiver presente, retorna falso imediatamente e cessa a checagem
+
+    bool neighborColours[corMax];  // array de booleanas para check-list de cada cor inferior
+
 
     // Itera pela lista de adjacência do vértice i, através do ponteiro "vizin". j é o número de vértices adjacentes a i;
-    for (int j = 1; j < vizin->size(); j++) {
+    for (int j = 0; j < vizin->size(); j++) {
+      int corVizin = vertices->getColour(vizin->get(j));  // recebe a cor do vizinho que se está checando
+
       // Checa se a cor dos vértices vizinhos é menor que a cor do vértice atual, corMax.
-      if (vertices->getColour(vizin->get(j)) < corMax) {
-        neighborColours[j] = true;  // caso a cor do vizinho seja inferior ao atual, marca na check-list booleana.
+      if (corVizin < corMax) {
+        neighborColours[corVizin] = true;  // caso a cor do vizinho seja inferior ao atual, marca na check-list booleana.
       }
     }
 
-    // Faz checkList de cores inferiores
-    int qtasCores = 0;
-
+    // Interrompe check-list e retorna falso se qualquer cor inferior a do vértice não estiver presente (nos vizinhos)
     for (int j = 0; j < corMax; j++) {
-      if (neighborColours[j] == true) {
-        qtasCores++;
+      if (neighborColours[j] == false) {
+        return false;
       }
     }
-
-    //
-    if (qtasCores < corMax - 1) {
-      // cout << " Out in " << ord[i][0] << " (" << qtasCores << "/" << corMax << ")" << endl;
-      return false;
-    } else {
-      i++;
-    }
+    i++;
   }
   return true;
 }
